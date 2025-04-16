28510afd8b35f8da6f801a1b32a7c18144eb13a7
at432 <S3820994@students.fscj.edu>
2025-04-16 10:33:47 -0400

    Added new message

--- hello.py
+++ hello.py
@@ -1,4 +1,11 @@
 # Alyssa Tolentino
 # 4/15/2025
 # A simple Python program to print a message
+
+# Alyssa Tolentino
+# 4/15/2025
+# A simple Python program to print a message
+
 print("Hello, World!")
+
+print("Welcome to GitHub!")
